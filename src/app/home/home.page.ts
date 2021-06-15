216f3e438558adace5eba75b2248cae87dd3a547
imanhafiz98 <imanhafiz333@gmail.com>
2021-06-15 11:54:05 +0800

    User can add new list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddNewTaskPage } from '../add-new-task/add-new-task.page';
 
 @Component({
   selector: 'app-home',
@@ -10,25 +12,43 @@ export class HomePage {
   todoList = [{
     itemName : 'Codimg',
     itemDueDate: '13-10-21',
-    itemPriority: 'high',
+    itemPriority: 'High',
     itemCategory : 'Work'
   },
   {
     itemName : 'Design',
     itemDueDate: '28-10-21',
-    itemPriority: 'low',
+    itemPriority: 'Low',
     itemCategory : 'Work'
   },
   {
     itemName : 'Shopping',
     itemDueDate: '13-10-21',
-    itemPriority: 'middle',
+    itemPriority: 'Middle',
+    itemCategory : 'Personal'
+  },
+  {
+    itemName : 'Workout',
+    itemDueDate: '13-10-21',
+    itemPriority: 'High',
     itemCategory : 'Personal'
   }
 ]
 
 today: number = Date.now();
 
-  constructor() {}
+  constructor(public modalCtrl:ModalController) {}
+
+  async addTask() {
+    const modal = await this.modalCtrl.create({
+      component: AddNewTaskPage
+    })
+
+    modal.onDidDismiss().then(newTaskObj =>{
+      console.log(newTaskObj.data);
+      this.todoList.push(newTaskObj.data)
+    })
+    return await modal.present()
+  }
 
 }
